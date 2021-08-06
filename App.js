619770a13d1f5b5098da2e23592a9d7c245aec01
Necralma <lihuel.almada@gmail.com>
2021-08-06 13:09:48 -0300

    tab fav

--- App.js
+++ App.js
@@ -3,12 +3,47 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import CoinsStack from "./src/screens/CoinsStack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Colors from './src/res/colors';
+import { Image, Text } from 'react-native';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-          <CoinsStack/>
+        <Tabs.Navigator
+          tabBarOptions={{
+            tintColor: "#fefefe",
+            style: { backgroundColor: Colors.blackPearl}
+          }}
+        >
+        <Tabs.Screen 
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size,color}) => {
+              <Image 
+              style={{tintColor: color, width:size, height:size}}
+              source={require('./src/assets/bank.png')} 
+              />  
+             }
+           }}
+        />
+        <Tabs.Screen 
+          name="Favorite"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size,color}) => {
+              <Image 
+              style={{tintColor: color, width:size, height:size}}
+              source={require('./src/assets/star.png')} 
+              />  
+             }
+           }}
+        />
+        </Tabs.Navigator>
       </NavigationContainer>
       <StatusBar/>
     </SafeAreaProvider>
--- src/screens/CoinsDetailScreen.js
+++ src/screens/CoinsDetailScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { View, Text, SectionList } from "react-native";
 
-
 class CoinsDetailScreen extends Component {
 
   state = {
